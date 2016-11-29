5ae27f5ad76bbb7aaee29eb8b8ae0ae6efb36118
KelvinM61 <g00311695@gmit.ie>
2016-11-29 12:53:11 +0000

    adding to database(not working)

--- webapp.py
+++ webapp.py
@@ -1,8 +1,10 @@
 # Kelvin Murphy
+#reference: https://github.com/data-representation/example-sqlite
 
-
+import sqlite3
 import flask as fl
 
+DATABASE = 'todolist.db'
 
 app = fl.Flask(__name__)
 
@@ -11,9 +13,29 @@ def root():
     return app.send_static_file('index.html')
 
 @app.route("/ToDo", methods=["GET", "POST"])
-def perms():
-	ToDo = (fl.request.values["userinput"])
-	return ToDo
+def todo():
+    ToDo = (fl.request.values["userinput"])
+    return ToDo
+    conn = sqlite3.connect('todolist.db')
+
+    c = conn.cursor()
+
+    # Create table
+    c.execute("INSERT into todolist(item) values(?)", [ToDo])
+    conn.commit()
+    
+    return ToDo
+
+@app.route("/store", methods=["GET", "POST"])
+def store():
+    conn = sqlite3.connect('todolist.db')
+
+    c = conn.cursor()
+
+    # Create table
+    c.execute("SELECT item from todolist")
+    return str(c.fetchall())
+
 
 if __name__ == "__main__":
     app.run()
