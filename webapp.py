99a1ded1ac7bfa20d748267bf648e10d9e877e81
KelvinM61 <g00311695@gmit.ie>
2016-11-30 15:43:32 +0000

    final commits

--- webapp.py
+++ webapp.py
@@ -41,9 +41,19 @@ def store():
 
     # Create table
     c.execute("SELECT * from todolist", (fl.request.form['item'],))
-    
+     print  ("\nTest from todolist.db")
+    l=[]
+    for a in g: l.append({'item':a['item']})
+        
+    print(l)
     return str(c.fetchall())
 
 
 if __name__ == "__main__":
     app.run()
+    
+    
+    
+    
+    
+    
